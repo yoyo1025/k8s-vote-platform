c2a6fbf3f1142892865aedb5583f18fb29761da1
yoyo1025 <hsjejxys@gmail.com>
2025-09-28 00:13:32 +0900

    feat: 型が使用できるかの確認

--- services/devmain/cmd/devmain/main.go
+++ services/devmain/cmd/devmain/main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 
+	resultv1 "github.com/yoyo1025/k8s-vote-platform/gen/go/result/v1"
 	"github.com/yoyo1025/k8s-vote-platform/libs/authjwt"
 )
 
 func main() {
 	fmt.Println("devmain:", authjwt.Stub())
+	req := &resultv1.PingRequest{}
+	_ = req
+	resp := &resultv1.PingResponse{Message: "pong(from generated type)"}
+	fmt.Println(resp.GetMessage())
 }
