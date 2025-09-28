001bb75ff25bd313a99ce7fcb2a710eff27a89b4
yoyo1025 <hsjejxys@gmail.com>
2025-09-28 18:47:43 +0900

    feat: gracefully stop実装

--- services/result-query/cmd/result-query/main.go
+++ services/result-query/cmd/result-query/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net"
+	"os"
+	"os/signal"
 
 	resultv1 "github.com/yoyo1025/k8s-vote-platform/gen/go/result/v1"
 	"github.com/yoyo1025/k8s-vote-platform/services/result-query/internal/server"
@@ -18,8 +20,16 @@ func main() {
 	s := grpc.NewServer()
 	resultv1.RegisterResultServiceServer(s, server.New())
 
-	log.Println("result-query gRPC listening on :50051")
-	if err := s.Serve(lis); err != nil {
-		log.Fatal(err)
-	}
+	go func() {
+		log.Println("result-query gRPC listening on :50051")
+		if err := s.Serve(lis); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
 }
