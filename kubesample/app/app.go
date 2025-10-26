36426dce5dfd0c825a368276d46742f065b24660
yoyo1025 <hsjejxys@gmail.com>
2025-10-26 14:42:37 +0900

    add: デバッグ要素追加

--- kubesample/app/app.go
+++ kubesample/app/app.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
 	e.GET("/ping", func(c echo.Context) error {
+		fmt.Println("pinged")
 		return c.JSON(http.StatusOK, map[string]interface{}{
 			"message": "pong",
 		})
